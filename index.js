cf19a655128775a76d4aaae23eeef89f4c36b03c
Hannes Diercks <github@xiphe.net>
2018-04-28 00:15:39 +0200

    fix(access): expose on main

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('./lib/cli/polyfills');
 require('draftlog').into(console);
 const publish = require('./lib/publish').default;
+const access = require('./lib/access').default;
 
 module.exports = {
   publish,
+  access,
 };

2ed54b56052e2294b3e3975598141c4ee3b2c359
Hannes Diercks <github@xiphe.net>
2018-05-07 10:42:43 +0200

    feat(publish): do not resolve publish task before finished

--- src/publish/monitorPublish.js
+++ src/publish/monitorPublish.js
@@ -17,7 +17,7 @@ const libraryVersionSubscription = gql`
 `;
 
 export default async function monitorPublish({ client, name, version, log }) {
-  client
+  return client
     .subscribe({
       query: libraryVersionSubscription,
       variables: {
@@ -26,56 +26,60 @@ export default async function monitorPublish({ client, name, version, log }) {
       },
     })
     .then((obs) => {
-      let latestDocVersion = -1;
-      const subscription = obs.subscribe({
-        next({ data: { libraryVersion } }) {
-          const {
-            getSourceFiles,
-            docVersion,
-            finished,
-            error,
-          } = libraryVersion;
+      return new Promise((resolve, reject) => {
+        let latestDocVersion = -1;
+        const subscription = obs.subscribe({
+          next({ data: { libraryVersion } }) {
+            const {
+              getSourceFiles,
+              docVersion,
+              finished,
+              error,
+            } = libraryVersion;
 
-          if (
-            log.steps.upload === STATE_PROGRESS &&
-            getSourceFiles === STATE_DONE
-          ) {
-            log.setSteps({
-              upload: STATE_DONE,
-            });
-          }
+            if (
+              log.steps.upload === STATE_PROGRESS &&
+              getSourceFiles === STATE_DONE
+            ) {
+              log.setSteps({
+                upload: STATE_DONE,
+              });
+            }
 
-          if (latestDocVersion >= docVersion) {
-            return;
-          }
+            if (latestDocVersion >= docVersion) {
+              return;
+            }
 
-          log.setSteps(
-            Object.keys(libraryVersion).reduce((memo, key) => {
-              if (log.steps[key]) {
-                return {
-                  ...memo,
-                  [key]: libraryVersion[key],
-                };
-              }
+            log.setSteps(
+              Object.keys(libraryVersion).reduce((memo, key) => {
+                if (log.steps[key]) {
+                  return {
+                    ...memo,
+                    [key]: libraryVersion[key],
+                  };
+                }
 
-              return memo;
-            }, {}),
-          );
+                return memo;
+              }, {}),
+            );
 
-          if (finished === STATE_DONE) {
-            subscription.unsubscribe();
-            process.exit();
-          }
+            if (finished === STATE_DONE) {
+              subscription.unsubscribe();
+              resolve();
+              process.exit();
+            }
 
-          if (error) {
-            log.error({ message: error });
-          }
+            if (error) {
+              log.error({ message: error });
+            }
 
-          latestDocVersion = docVersion;
-        },
-        error(err) {
-          log.error(err);
-        },
+            latestDocVersion = docVersion;
+          },
+          error(err) {
+            reject(err);
+            log.error(err);
+          },
+        });
       });
     });
 }

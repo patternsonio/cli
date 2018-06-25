1f1a555218c3618e45b854a91ef49e4049f8f28f
Hannes Diercks <github@xiphe.net>
2018-06-25 14:28:10 +0200

    fix(polyfills): explicitly handle promise rejections
    includes a workaround for a rejection that does not seem problematic but
    I can not find the cause for

--- src/cli/polyfills.js
+++ src/cli/polyfills.js
@@ -5,4 +5,18 @@ const fs = require('fs');
 // TODO: remove once fs.promises is stable
 delete fs.promises;
 
+process.on('unhandledRejection', (reason) => {
+  // TODO: find out what is causing this rejection and remove this workaround
+  // Probably s.th. with typescript + virtual file system
+  if (
+    reason ===
+    'The "path" argument must be of type string. Received type object'
+  ) {
+    return;
+  }
+  // eslint-disable-next-line no-console
+  console.error(reason, reason.stack);
+  process.exit(1);
+});
+
 global.regeneratorRuntime = global.regeneratorRuntime || runtime;

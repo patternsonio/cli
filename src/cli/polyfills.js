444abe0792fdfea724c12d887ffa6a0e74915ade
Hannes Diercks <github@xiphe.net>
2018-06-25 14:26:32 +0200

    fix(polyfills): remove experimental promises from fs
    in order to not cause warnings for users of the cli

--- src/cli/polyfills.js
+++ src/cli/polyfills.js
@@ -1,3 +1,8 @@
-import runtime from 'babel-runtime/regenerator';
+const runtime = require('babel-runtime/regenerator');
+const fs = require('fs');
+
+// Prevent other libs from using experimental API and causing a warning
+// TODO: remove once fs.promises is stable
+delete fs.promises;
 
 global.regeneratorRuntime = global.regeneratorRuntime || runtime;

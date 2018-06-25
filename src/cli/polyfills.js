ca86395be991714a633652bc8cc7a813571219c6
Hannes Diercks <github@xiphe.net>
2018-06-25 15:22:18 +0200

    fix(polyfills): ignore another variant of the path error

--- src/cli/polyfills.js
+++ src/cli/polyfills.js
@@ -1,5 +1,6 @@
 const runtime = require('babel-runtime/regenerator');
 const fs = require('fs');
+const { red } = require('../colors');
 
 // Prevent other libs from using experimental API and causing a warning
 // TODO: remove once fs.promises is stable
@@ -9,13 +10,15 @@ process.on('unhandledRejection', (reason) => {
   // TODO: find out what is causing this rejection and remove this workaround
   // Probably s.th. with typescript + virtual file system
   if (
-    reason ===
-    'The "path" argument must be of type string. Received type object'
+    [
+      'Path must be a string. Received null',
+      'The "path" argument must be of type string. Received type object',
+    ].includes(reason)
   ) {
     return;
   }
   // eslint-disable-next-line no-console
-  console.error(reason, reason.stack);
+  console.error(red('Error:'), reason, reason.stack ? reason.stack : '');
   process.exit(1);
 });
 
